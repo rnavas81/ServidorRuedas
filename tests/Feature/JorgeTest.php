28a45ef61d13f466771c107944096dee6bb98a39
Under <jorgeolmo.I@gmail.com>
2021-03-09 19:55:23 +0100

    Arreglo test

--- tests/Feature/JorgeTest.php
+++ tests/Feature/JorgeTest.php
@@ -15,47 +15,40 @@ class JorgeTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
-    {
-        $response = $this->get('/');
-
+    
+    public function test_Devuelve_el_rol_correctamente(){
+        $response = $this->post('api/login', ['email' => 'carshare.ifpvdg@gmail.com', 'password' => 'Chubaca2020']);
+        
+        $token = $response->json();
+        $token = $token['access_token'];
+        
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '. $token,
+        ])->post('api/usuario/testRol');
+        
         $response->assertStatus(200);
     }
     
-    public function test_crearUsuario(){
-        $user = new User;
-        $user->name = 'User';
-        $user->surname = 'surname';
-        $user->email = 'mail@nomail.com';
-        $user->password = bcrypt('123456');
-        $user->save();
-        
-        $this->assertEquals('User', $user->name);
-        $this->assertEquals('surname', $user->surname);
-        $this->assertEquals('mail@nomail.com', $user->email);
-        $this->assertEquals(0, $user->status);
-    }
-    
-    public function test_login() {
+    public function test_Se_hace_login_correctamente() {
         $response = $this->post('api/login', ['email' => 'carshare.ifpvdg@gmail.com', 'password' => 'Chubaca2020']);
         
         
         $response->assertStatus(200);
     }
     
-    public function test_recuperar() {
+    public function test_Recuperar_contraseña_genera_nueva_pass() {
         $response = $this->post('api/forget', ['email' => 'mail1@nomail.com']);
         
         $response->assertStatus(200);
     }
     
-    public function test_alfanumerico(){
+    public function test_funcion_alfanumerico_genera_correctamente(){
         $string = app(AuthController2::class)->generarAlfanumerico(0,15);
        
         $this->assertEquals('15', strlen($string));
     }
     
-    public function test_token() {
+    public function test_se_comprueba_que_el_token_es_correcto() {
         $response = $this->post('api/login', ['email' => 'carshare.ifpvdg@gmail.com', 'password' => 'Chubaca2020']);
         
         $token = $response->json();
